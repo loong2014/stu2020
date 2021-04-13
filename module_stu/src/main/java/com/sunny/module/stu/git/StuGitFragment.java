f9be53c7bf93748166083939609bff0c6dd8ed25
zhangxin17 <zhangxin17@le.com>
2021-04-13 11:01:08 +0800

    add git stu page

--- module_stu/src/main/java/com/sunny/module/stu/git/StuGitFragment.java
+++ module_stu/src/main/java/com/sunny/module/stu/git/StuGitFragment.java
@@ -7,8 +7,12 @@ public class StuGitFragment extends StuBaseFragment {
 
     /**
      * https://blog.csdn.net/qq_32452623/article/details/78276451
-     *
+     * <p>
      * https://www.jianshu.com/p/89c3d2fb1a66
+     * <p>
+     * https://www.jianshu.com/p/5068997164cb
+     * <p>
+     * https://www.jianshu.com/p/5068997164cb
      */
 
     @Override
