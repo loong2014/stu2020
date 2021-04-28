587dbfee1b5a0d9208d84a25bb0299808e083fa6
zhangxin17 <zhangxin17@le.com>
2021-04-28 20:06:29 +0800

    add stu constraintlayout

---
+++ module_stu/src/main/java/com/sunny/module/stu/layout/StuConstraintLayoutFragment.java
@@ -0,0 +1,19 @@
+package com.sunny.module.stu.layout;
+
+import com.sunny.module.stu.R;
+import com.sunny.module.stu.base.StuBaseFragment;
+
+public class StuConstraintLayoutFragment extends StuBaseFragment {
+
+    /**
+     * https://cloud.tencent.com/developer/article/1768742?from=article.detail.1176302
+     * <p>
+     * https://cloud.tencent.com/developer/article/1176302
+     */
+
+
+    @Override
+    protected int buildLayoutResID() {
+        return R.layout.fragment_stu_constraint_layout;
+    }
+}
